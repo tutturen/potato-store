7b28aa220532838bb17681ea717eec5e65a0be5a
Thor Even Tutturen <thor@bustbyte.no>
2018-02-20 15:41:50 +0100

    add graphene-django

--- src/products/models.py
+++ src/products/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    notes = models.TextField()
+    category = models.ForeignKey(Category,
+        related_name='products',
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
---
+++ src/products/schema.py
@@ -0,0 +1,27 @@
+import graphene
+
+from graphene_django.types import DjangoObjectType
+
+from products.models import Category, Product
+
+
+class CategoryType(DjangoObjectType):
+    class Meta:
+        model = Category
+
+
+class ProductType(DjangoObjectType):
+    class Meta:
+        model = Product
+
+
+class Query(object):
+    all_categories = graphene.List(CategoryType)
+    all_products = graphene.List(ProductType)
+
+    def resolve_all_categories(self, info, **kwargs):
+        return Category.objects.all()
+
+    def resolve_all_products(self, info, **kwargs):
+        # We can easily optimize query count in the resolve method
+        return Product.objects.select_related('category').all()
