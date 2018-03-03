b06d2d4054db882c7222fd37b8a725a0e11761d9
Thorben Werner Sjøstrøm Dahl <thorben@sjostrom.no>
2018-03-03 16:31:54 +0100

    Fix CSRF token not being sent across origins

--- src/potatostore/urls.py
+++ src/potatostore/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.decorators.csrf import ensure_csrf_cookie
 from django.views.generic import TemplateView
 
 from graphene_django.views import GraphQLView
@@ -7,5 +8,7 @@ from graphene_django.views import GraphQLView
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^graphql', GraphQLView.as_view(graphiql=True)),
-    url(r'^$', TemplateView.as_view(template_name='potatostore/index.html')),
+    url(r'^$', ensure_csrf_cookie(
+        TemplateView.as_view(template_name='potatostore/index.html')
+    )),
 ]
