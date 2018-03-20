7692a0da7a608459834a3dde3ead22cbcff4e940
erlendjs <erlend@erlendjs.no>
2018-03-20 12:41:42 +0100

    Update one more file.

--- src/potatostore/schema.py
+++ src/potatostore/schema.py
@@ -1,12 +1,15 @@
 import graphene
 import products.schema
 
+
 class Query(products.schema.Query, graphene.ObjectType):
     # This class will inherit from multiple Queries
     # as we begin to add more apps to our project
     pass
 
+
 class Mutation(products.schema.Mutation, graphene.ObjectType):
     pass
+
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
