ed889b0de46b13ccf5f7510deb4de4d917274362
Håvard Bjerke <h0wy36@gmail.com>
2018-03-23 15:24:41 +0100

    - Fix diverged migrations

--- src/products/migrations/0012_auto_20180323_1344.py
+++ src/products/migrations/0012_auto_20180323_1344.py
@@ -11,38 +11,10 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RemoveField(
-            model_name='product',
-            name='packageDeal',
-        ),
-        migrations.RemoveField(
-            model_name='product',
-            name='percentSale',
-        ),
         migrations.AddField(
             model_name='product',
             name='stockValue',
             field=models.FloatField(default=1, verbose_name='Stock value'),
             preserve_default=False,
-        ),
-        migrations.AlterField(
-            model_name='packagedeal',
-            name='minimumQuantity',
-            field=models.IntegerField(verbose_name='You buy'),
-        ),
-        migrations.AlterField(
-            model_name='packagedeal',
-            name='paidQuantity',
-            field=models.IntegerField(verbose_name='You pay for'),
-        ),
-        migrations.AlterField(
-            model_name='packagedeal',
-            name='product',
-            field=models.ManyToManyField(related_name='package_deal', to='products.Product'),
-        ),
-        migrations.AlterField(
-            model_name='percentsale',
-            name='product',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='percent_sale', to='products.Product'),
         ),
     ]
